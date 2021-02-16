b936fe40450d89b43cd99f6301027f1f336801bf
Arya <227897+petewarrior@users.noreply.github.com>
2021-02-17 00:42:28 +0700

    can now handle channel direct url

--- youtube-subscribers.py
+++ youtube-subscribers.py
@@ -4,56 +4,92 @@ import datetime
 
 import requests
 
-def get_statistics(ids=list, names=list):
+def append_statistics(stats):
+    def f(c):
+        s = next((i for i in stats if i["id"] == c["channel_url"]), None)
+        if s is not None:
+            c['statistics'] = s['statistics']
+        return c
+    return f
+
+
+def get_statistics(channels=list):
+    ids = list(map(lambda c: c["channel_url"], channels))
     idlist = ','.join(ids)
 
     res = []
 
-    if len(ids) > 0 :
-      params = {'part': 'statistics,status', 'id': idlist, 'key': "YOUR YOUTUBE API KEY HERE"}
-      r = requests.get("https://youtube.googleapis.com/youtube/v3/channels", params)
+    if len(ids) > 0:
+        params = {'part': 'statistics,status', 'id': idlist,
+                  'key': "youtube api key here"}
+        r = requests.get(
+            "https://youtube.googleapis.com/youtube/v3/channels", params)
 
-      response = r.json()
-      res = res + response["items"]
-    
-    return res 
+        response = r.json()
+        res = res + response["items"]
 
-def main() :
-  site = pywikibot.Site()
+    app = append_statistics(res)
 
-  # PAGE NAMES TO MONITOR HERE
-  pages = []
+    return map(app, channels)
 
-  for p in pages:
-    page = pywikibot.Page(site, p)
-    text = page.text
 
-    channel_ids = re.findall(r'(?:\{\{Infobox YouTube personality).*?(?:channel_url).*?\=.*?([\w_-]+)', text, flags=re.DOTALL)
+def main():
+    site = pywikibot.Site()
 
-    stats = get_statistics(ids= channel_ids)
+    # pages values can either be the page title as string or a dictionary in the following format:
+    # { 'page': '<page title>', channels: [{'channel_direct_url': 'c/omit_if_inapplicable', 'channel_url': 'required'}]}
+    pages = []
 
-  for s in stats:
-      id = s["id"]
-      viewsInt = int(s["statistics"]["viewCount"])
-      views =  "{:,}".format(viewsInt)
+    for p in pages:
+        if isinstance(p, str):
+            page_title = p
+        if isinstance(p, dict):
+            page_title = p["page"]
 
-      subscribersInt = int(s["statistics"]["subscriberCount"])
-      subscribers = ('~' if subscribersInt > 100000 else '') + "{:,}".format(subscribersInt)
+        page = pywikibot.Page(site, page_title)
+        text = page.text
 
-      subcomp = re.compile(r'(\{\{Infobox YouTube personality.*?channel_url.*?\=.*?'+ id +r'.*?subscribers.*?\=.*?)([\w\s\~\.\,]+)[\n\s]*', flags=re.DOTALL)
+        if isinstance(p, str):
+            channel_ids = re.findall(
+                r'(?:\{\{Infobox YouTube personality).*?(?:channel_url).*?\=.*?([\w_-]+)', text, flags=re.DOTALL)
+            channels = list(map(lambda ch: {"channel_url": ch}, channel_ids))
+        else:
+            channels = p["channels"]
 
-      # subscribers
-      text = subcomp.sub(r'\1 ' + subscribers + r"\n", text)
+        stats = get_statistics(channels=channels)
 
-      # views
-      text = re.sub(r'(\{\{Infobox YouTube personality.*?channel_url.*?\=.*?'+ id + r'.*?views.*?\=.*?)([\w\s\.\,]+)[\n\s]*', r"\1 " + views + r"\n", text, flags=re.DOTALL)
+        for s in stats:
+            if "channel_direct_url" in s and isinstance(s['channel_direct_url'], str):
+                channel_type = 'channel_direct_url'
+                id = s["channel_direct_url"]
+            else:
+                channel_type = 'channel_url'
+                id = s["channel_url"]
+            viewsInt = int(s["statistics"]["viewCount"])
+            views = "{:,}".format(viewsInt)
 
-      # stats_update
-      text = re.sub(r'(\{\{Infobox YouTube personality.*?channel_url.*?\=.*?' + id + r'.*?stats_update.*?\=.*?)({{date\|[\d-]+}})[\n\s]*', r"\1" + datetime.datetime.now().strftime("{{date|%Y-%m-%d}}") + r"\n", text, flags=re.DOTALL)
+            subscribersInt = int(s["statistics"]["subscriberCount"])
+            subscribers = ('~' if subscribersInt > 100000 else '') + \
+                "{:,}".format(subscribersInt)
 
-      page.text = text
+            subcomp = re.compile(r'(\{\{Infobox YouTube personality.*?' + channel_type + r'.*?\=.*?' +
+                                 id + r'.*?subscribers.*?\=.*?)([\w\s\~\.\,]+)[\n\s]*', flags=re.DOTALL)
 
-    page.save(summary= "YouTube subscriber update script, ran manually")
+            # subscribers
+            text = subcomp.sub(r'\1 ' + subscribers + r"\n", text)
+
+            # views
+            text = re.sub(r'(\{\{Infobox YouTube personality.*?' + channel_type + r'.*?\=.*?' + id +
+                          r'.*?views.*?\=.*?)([\w\s\.\,]+)[\n\s]*', r"\1 " + views + r"\n", text, flags=re.DOTALL)
+
+            # stats_update
+            text = re.sub(r'(\{\{Infobox YouTube personality.*?' + channel_type + r'.*?\=.*?' + id +
+                          r'.*?stats_update.*?\=.*?)({{date\|[\d-]+}})[\n\s]*', r"\1" + datetime.datetime.now().strftime("{{date|%Y-%m-%d}}") + r"\n", text, flags=re.DOTALL)
+
+            page.text = text
+
+        page.save(summary="YouTube subscriber update")
+
 
 if __name__ == "__main__":
-  main()
+    main()
