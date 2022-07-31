dca746cc0f9eb49eac57c5568d5e0166c3ef96f2
Arya Antaputra <petewarrior@gmail.com>
2022-07-31 18:38:26 +0700

    wikidata update is working

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # pywikibot-youtube-subscribers
-A simple script to update YouTube channel stats in a Wikipedia page. 
+A simple script to update YouTube channel stats in a Wikidata page. Inspired by Borkbot. 
 
 Basically, this script does three things:
-1. Parse a [YouTube personality infobox](https://en.wikipedia.org/wiki/Template:Infobox_YouTube_personality) on a Wikipedia page to obtain the channel ID 
-2. Query the statistics for the channel ID to the YouTube API 
-3. Update the stats in the infobox
+1. Get all YouTube channel ID (P2397) values in a Wikidata page
+2. Query the statistics for each channel ID to the YouTube API 
+3. Update the stats in Wikidata
 
 Requires [Pywikibot](https://www.mediawiki.org/wiki/Manual:Pywikibot), Python 3, and a [YouTube API key](https://developers.google.com/youtube/v3/getting-started).
 
-## Page requirements
-* The page must have a Wikipedia YouTube personality infobox, either on its own or as a module in another infobox.
-* The script can only parse YouTube channel ID in the ```channel_url``` field. If the infobox uses ```channel_direct_url```, both ```channel_id``` and ```channel_direct_url``` must be provided in the script (example in source code). Only one channel per infobox is supported.
-* Last update time is printed in the ```stats_update``` field. It must already exist and have a date formatted using the [Wikipedia date template](https://en.wikipedia.org/wiki/Template:Date) with the ```YYYY-MM-DD``` format.
+## Displaying stats on Wikipedia page
+Replace the `subscribers`, `views`, and `stats_update` fields in the Youtube personality infobox as follows. If there are more than one channel, the stats will be displayed as a list.
+```
+| subscribers = {{#invoke:WikidataIB|getValue|P2397|qo=yes|qual=P3744|list=ubl|fetchwikidata=ALL|onlysourced=no|scale=a|noicon=yes}}
+| views = {{#invoke:WikidataIB|getValue|P2397|qo=yes|qual=P5436|list=ubl|fetchwikidata=ALL|onlysourced=no|scale=a|noicon=yes}}
+| stats_update = {{#invoke:WikidataIB|getValue|P2397|qo=yes|qual=P585|maxvals=1|list=ubl|fetchwikidata=ALL|onlysourced=no|noicon=no}}
+```
--- youtube-subscribers-wikidata.py
+++ youtube-subscribers-wikidata.py
@@ -2,6 +2,9 @@ import time
 import pywikibot
 import re
 import datetime
+import os
+
+from dotenv import load_dotenv
 
 import requests
 
@@ -22,7 +25,7 @@ def get_statistics(channels=list):
 
     if len(ids) > 0:
         params = {'part': 'statistics,status', 'id': idlist,
-                  'key': "youtube_api_key"}
+                  'key': os.environ.get("YOUTUBE_API_KEY")}
         r = requests.get(
             "https://youtube.googleapis.com/youtube/v3/channels", params)
 
@@ -41,47 +44,58 @@ def main():
     site = pywikibot.Site("wikidata", "wikidata")
     repo = site.data_repository()
 
-    pages = []
+    load_dotenv()
+
+    pages = os.environ.get('WIKIDATA_IDS', '').split(',')
 
     for p in pages:
-        page = pywikibot.Page(wiki, p)
-        item = pywikibot.ItemPage.fromPage(page)
+        item = pywikibot.ItemPage(site, p)
 
         dictionary = item.get()
 
-        if dictionary['claims']['P2397'] is None : continue
-        claim = dictionary['claims']['P2397'][0]
+        if dictionary['claims']['P2397'] is None : continue        
 
-        subscribers = claim.qualifiers['P3744'][0] if claim.qualifiers['P3744'] else None
-        views = claim.qualifiers['P5436'][0] if claim.qualifiers['P5436'] else None
-        pit = claim.qualifiers['P585'][0] if claim.qualifiers['P585'] else None
-        channel = claim.target
+        for c in range(len(dictionary['claims']['P2397'])):
+            claim = dictionary['claims']['P2397'][c]
 
-        channels = [{'channel_url': channel}]
+            subscribers = claim.qualifiers['P3744'][0] if 'P3744' in claim.qualifiers else None
+            views = claim.qualifiers['P5436'][0] if 'P5436' in claim.qualifiers else None
+            pit = claim.qualifiers['P585'][0] if 'P585' in claim.qualifiers else None
+            videos = claim.qualifiers['P3740'][0] if 'P3740' in claim.qualifiers else None
+            channel = claim.target
 
-        stats = get_statistics(channels=channels)
-        
-        for s in stats:
+            channels = [{'channel_url': channel}]
 
-          viewsInt = s["statistics"]["viewCount"]
-          subscribersInt = s["statistics"]["subscriberCount"]
-          
-          subsCount = pywikibot.WbQuantity(amount='+'+subscribersInt, site=repo)
-          newSubscribers = pywikibot.Claim(repo, u'P3744') # number of subscribers
-          newSubscribers.setTarget(subsCount)
-          viewCount = pywikibot.WbQuantity(amount='+'+viewsInt, site=repo)
-          newViews = pywikibot.Claim(repo, u'P5436') # number of subscribers
-          newViews.setTarget(viewCount)
-          currTime = datetime.datetime.now()
-          piTime = pywikibot.WbTime(int(currTime.strftime('%Y')), int(currTime.strftime('%m')), int(currTime.strftime('%d')))
-          newPit = pywikibot.Claim(repo, u'P585') # number of subscribers
-          newPit.setTarget(piTime)
-          
+            stats = get_statistics(channels=channels)
+            
+            for s in stats:
 
-          claim.removeQualifiers([subscribers, views, pit])
-          claim.addQualifier(newSubscribers, summary=u'Update subscriber count')
-          claim.addQualifier(newViews, summary=u'Update view count')
-          claim.addQualifier(newPit, summary=u'update point in time')
+                viewsInt = s["statistics"]["viewCount"]
+                subscribersInt = s["statistics"]["subscriberCount"]
+                videosInt = s["statistics"]["videoCount"]
+                
+                subsCount = pywikibot.WbQuantity(amount='+'+subscribersInt, site=repo)
+                newSubscribers = pywikibot.Claim(repo, u'P3744') # number of subscribers
+                newSubscribers.setTarget(subsCount)
+                viewCount = pywikibot.WbQuantity(amount='+'+viewsInt, site=repo)
+                newViews = pywikibot.Claim(repo, u'P5436') # number of views
+                newViews.setTarget(viewCount)
+                videoCount = pywikibot.WbQuantity(amount='+'+videosInt, site=repo)
+                newVideos = pywikibot.Claim(repo, u'P3740') # number of videos
+                newVideos.setTarget(videoCount)
+                currTime = datetime.datetime.now()
+                piTime = pywikibot.WbTime(int(currTime.strftime('%Y')), int(currTime.strftime('%m')), int(currTime.strftime('%d')))
+                newPit = pywikibot.Claim(repo, u'P585') # point in time
+                newPit.setTarget(piTime)
+            
+            if subscribers: claim.removeQualifier(subscribers)
+            if views: claim.removeQualifier(views)
+            if pit: claim.removeQualifier(pit)
+            if videos: claim.removeQualifier(videos)
+            claim.addQualifier(newSubscribers, summary=u'Update subscriber count')
+            claim.addQualifier(newViews, summary=u'Update view count')
+            claim.addQualifier(newVideos, summary=u'update videos count')
+            claim.addQualifier(newPit, summary=u'update point in time')
 
 if __name__ == "__main__":
     main()
