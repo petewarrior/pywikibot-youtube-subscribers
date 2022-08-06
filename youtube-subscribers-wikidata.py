9e6c6997966fa926b5096f58e439969f691f44fd
Arya Antaputra <petewarrior@gmail.com>
2022-08-06 18:41:26 +0700

    populate new entries, add minimal change to update

--- youtube-subscribers-wikidata.py
+++ youtube-subscribers-wikidata.py
@@ -1,18 +1,19 @@
-import time
 import pywikibot
-import re
 import datetime
 import os
+from unicodedata import normalize
+from dateutil.parser import parse
 
 from dotenv import load_dotenv
 
 import requests
 
-def append_statistics(stats):
+def append_data(stats, key=['statistics']):
     def f(c):
         s = next((i for i in stats if i["id"] == c["channel_url"]), None)
         if s is not None:
-            c['statistics'] = s['statistics']
+            for k in key :
+                c[k] = s[k]
         return c
     return f
 
@@ -24,7 +25,7 @@ def get_statistics(channels=list):
     res = []
 
     if len(ids) > 0:
-        params = {'part': 'statistics,status', 'id': idlist,
+        params = {'part': 'snippet,statistics', 'id': idlist,
                   'key': os.environ.get("YOUTUBE_API_KEY")}
         r = requests.get(
             "https://youtube.googleapis.com/youtube/v3/channels", params)
@@ -32,12 +33,30 @@ def get_statistics(channels=list):
         response = r.json()
         res = res + response["items"]
 
-        app = append_statistics(res)
+        app = append_data(res, ['snippet','statistics'])
 
         return map(app, channels)
 
     return []
 
+def get_playlist_statistics(playlists=list) :
+    ids = list(map(lambda c: c["channel_url"], playlists))
+    idlist = ','.join(ids)
+
+    if len(ids) > 0:
+        params = {'part': 'snippet,contentDetails,status', 'id': idlist,
+                  'key': os.environ.get("YOUTUBE_API_KEY")}
+        r = requests.get(
+            "https://youtube.googleapis.com/youtube/v3/playlists", params)
+
+        response = r.json()
+        res = res + response["items"]
+
+        app = append_data(res, ['snippet','statistics'])
+
+        return map(app, playlists)
+
+    return []
 
 def main():
     wiki = pywikibot.Site("en", "wikipedia")
@@ -53,20 +72,23 @@ def main():
 
         dictionary = item.get()
 
-        if dictionary['claims']['P2397'] is None : continue        
+        if 'P2397' not in dictionary['claims'] : continue        
         print(dictionary['labels']['en'])
         
         for c in range(len(dictionary['claims']['P2397'])):
             claim = dictionary['claims']['P2397'][c]
             channel_id = claim.getTarget()
-            if u'P3744' in claim.qualifiers :
-                print(claim.qualifiers['P1810'][0].getTarget())
+            current_channel_name = claim.qualifiers['P1810'][0] if u'P1810' in claim.qualifiers else None
+            
+            if current_channel_name :
+                print(current_channel_name.getTarget())
             else :
-                print(channel_id)
+                print(channel_id + u' (will populate channel name)')
             if claim.getRank() == 'deprecated' :
                 print('Skipping deprecated entry')
                 continue
             
+            start_date = claim.qualifiers['P580'][0] if 'P580' in claim.qualifiers else None
             subscribers = claim.qualifiers['P3744'][0] if 'P3744' in claim.qualifiers else None
             views = claim.qualifiers['P5436'][0] if 'P5436' in claim.qualifiers else None
             pit = claim.qualifiers['P585'][0] if 'P585' in claim.qualifiers else None
@@ -78,10 +100,11 @@ def main():
             stats = get_statistics(channels=channels)
 
             for s in stats: # always one for now
-
                 viewsInt = s["statistics"]["viewCount"]
                 subscribersInt = s["statistics"]["subscriberCount"]
                 videosInt = s["statistics"]["videoCount"]
+                channelNameString = s["snippet"]["title"]
+                publishedAt = parse(s["snippet"]["publishedAt"])
                 
                 subsCount = pywikibot.WbQuantity(amount='+'+subscribersInt, site=repo)
                 newSubscribers = pywikibot.Claim(repo, u'P3744') # number of subscribers
@@ -96,7 +119,52 @@ def main():
                 piTime = pywikibot.WbTime(int(currTime.strftime('%Y')), int(currTime.strftime('%m')), int(currTime.strftime('%d')))
                 newPit = pywikibot.Claim(repo, u'P585') # point in time
                 newPit.setTarget(piTime)
+
+            if pit :
+                if pit.getTarget().toTimestr() == piTime.toTimestr() :
+                    print('Last updated today, skipping')
+                    continue
+            
+            if subscribers :
+                if(int(subscribersInt) < 100000) :
+                    if abs(int(subscribers.getTarget().amount) - int(subscribersInt)) < 1000 :
+                        print('Subscriber change <1000, skipping')
+                        continue
+                elif(int(subscribersInt) < 1000000) :
+                    if abs(int(subscribers.getTarget().amount) - int(subscribersInt)) < 5000 :
+                        print('Silver, subscriber change <5000, skipping')
+                        continue
+                else :
+                    if abs(float(subscribers.getTarget().amount) - float(subscribersInt)) / float(subscribers.getTarget().amount) < 0.01 :
+                        print('Gold or higher, subscriber change <1%, skipping')
+                        continue
+
+            try :
+                if current_channel_name is not None :
+                    if channelNameString.strip() != current_channel_name.getTarget().strip() :
+                        print('Removing old channel name')
+                        claim.removeQualifier(current_channel_name, summary=u'Remove old channel name')
+                        current_channel_name = None
+            except BaseException as err :
+                print(err)
+
+            try :
+                if current_channel_name is None :
+                    print('Adding channel name: "' + channelNameString + '"')
+                    newChannelName = pywikibot.Claim(repo, u'P1810')
+                    newChannelName.setTarget(channelNameString.strip())
+                    claim.addQualifier(newChannelName, summary=u'Set channel name')
+            except BaseException as err :
+                print('Adding channel name error, skipped')
+                print(err)
                 
+            if start_date is None :
+                print('Adding start date')
+                start_date_datetime = pywikibot.WbTime(int(publishedAt.strftime('%Y')), int(publishedAt.strftime('%m')), int(publishedAt.strftime('%d')))
+                newStartDate = pywikibot.Claim(repo, u'P580')
+                newStartDate.setTarget(start_date_datetime)
+                claim.addQualifier(newStartDate, summary=u'Add start time')
+
             print('Removing old qualifiers')
             qualifiersToRemove = []
             if(subscribers) : qualifiersToRemove.append(subscribers)
@@ -117,7 +185,7 @@ def main():
             newPit.setTarget(piTime)
 
             print('Add stats record')
-            oldstats = dictionary['claims']['P8687']
+            oldstats = dictionary['claims']['P8687'] if u'P8687' in dictionary['claims'] else []
 
             for r in oldstats:
                 if u'P2397' in r.qualifiers:
